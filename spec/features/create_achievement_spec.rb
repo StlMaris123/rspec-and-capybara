63c4f853c7b20f5bee5f8e27e148ed0f31ef0ded
StlMaris123 <stellamarisnjage@gmail.com>
2017-01-30 10:58:19 +0300

    feature spec for happy path

--- spec/features/create_achievement_spec.rb
+++ spec/features/create_achievement_spec.rb
@@ -4,11 +4,11 @@ feature 'create new achievement' do
   scenario 'create new achievement with valid data' do
     visit('/')
     click_on('New Achievement')
-    fill_n('Title', with: 'Read a book')
+    fill_in('Title', with: 'Read a book')
     fill_in('Description', with: 'Excsllent read')
     select('Public', from: 'Privacy')
     check('Featured achievement')
-    atach_file('Cover image', "#{Rails.root}/spec/fixtures/cover_image.png")
+    attach_file('Cover image', "#{Rails.root}/spec/fixtures/cover_image.png")
     click_on('Create Achievement')
 
     expect(page).to have_content('Achievement has been created')
