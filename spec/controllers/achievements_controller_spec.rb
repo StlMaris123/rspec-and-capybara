9797b1f4bd131ecf7f591375199ece361c99015c
StlMaris123 <stellamarisnjage@gmail.com>
2017-02-02 15:31:49 +0300

    controller test for the new action

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe AchievementsController, type: :controller do
+  describe "Get new" do
+    it"renders :new template " do
+      get :new
+      expect(response).to render_template(:new)
+    end
+
+    it "assigns new Achievement to @achievement" do
+      get :new
+      expect(assigns(:achievement)).to be_a_new(Achievement)
+    end
+  end
 
 end
